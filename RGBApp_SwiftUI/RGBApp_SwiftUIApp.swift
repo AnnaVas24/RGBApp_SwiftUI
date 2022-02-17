a6b4c79f5feb49c377d9ce7a6d83442a72f3c38d
Anna <anna.vasi4ko@icloud.com>
2022-02-17 23:14:29 +0300

    Initial Commit

---
+++ RGBApp_SwiftUI/RGBApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBApp_SwiftUIApp.swift
+//  RGBApp_SwiftUI
+//
+//  Created by Vasichko Anna on 17.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
